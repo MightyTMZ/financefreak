faecd3bf6f4b31f3f40e1bbb6140ad2b84e32723
Tom Zhang <tommyawesome08@gmail.com>
2025-06-11 13:47:58 -0400

    open graph image for FinanceFreak

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -10,6 +10,16 @@ const inter = Inter({ subsets: ['latin'] });
 export const metadata: Metadata = {
   title: 'FinanceFreak - Virtual Stock Market Game',
   description: 'Trade virtual stocks with real market data in this stock market simulation game',
+  openGraph: {
+    images: [
+      {
+        url: "https://financefreak.vercel.app/og-image/ff-og-image.jpg",
+        width: 1200,
+        height: 630,
+        alt: "Azure Partners - Offical Website",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
